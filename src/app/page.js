e3130a4217be978962b85caff1c657a5d598fc76
Praveen Kumar <cbkmar92@gmail.com>
2024-03-28 16:40:57 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,15 @@
 import Image from "next/image";
+import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
+       
         <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+          <button className="btn btn-primary">bootstrap button</button>
+     
         </div>
       </div>
 
