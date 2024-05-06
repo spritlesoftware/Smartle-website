2131100a1e7f654ca18a2b7d07d4a884071fc1a5
janani_g <janani.g@spritle.com>
2024-05-06 11:57:44 +0530

    removed the config file

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  API_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqZ2toanZ4a3ZrY3JxYW1scm5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE5NzI4MDQsImV4cCI6MjAyNzU0ODgwNH0.6gjvj2IYDuvvaCV0UDwZkVg51LJ7rvLx-CKT-ROpNqk"
-};
-
-// Export the config object
-export default config;
-
