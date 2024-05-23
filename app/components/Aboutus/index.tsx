19f77eef80f1594c69b35238edc20bccc49c293e
navanee1609 <navaneethanvs18@gmail.com>
2024-05-23 15:32:05 +0530

    Refactored Aboutus page with new design and content, replacing existing layout and components with updated styles and elements.

--- app/components/Aboutus/index.tsx
+++ app/components/Aboutus/index.tsx
@@ -34,11 +34,32 @@ const Aboutus = () => {
     return (
 
         <div>
+            <div className='mx-auto max-w-7xl px-4 py-24 my-32 lg:px-10 bg-lightgrey rounded-3xl relative'>
+                <Image src="/images/aboutus/dots.svg" width={100} height={100} alt="dots-image" className="absolute bottom-1 -left-20" />
+                <h3 className='text-center text-blue text-lg tracking-widest'>SMARTLE.AI</h3>
+                <h4 className='text-center text-4xl lg:text-65xl font-bold'>Your AI Partner</h4>
+                <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 my-16 gap-x-16 lg:gap-x-32'>
+                    {Aboutdata.map((item, i) => (
+                        <div key={i} className='bg-white rounded-3xl mt-16 pt-10 pl-8 pb-10 pr-6 shadow-xl group card'>
+                            <h4 className='text-4xl font-semibold  text-black mb-5 group-hover:text-white'>{item.heading}</h4>
+                            {/* <Image src={item.imgSrc} alt={item.imgSrc} width={100} height={100} className="mb-5" /> */}
+                            <h4 className='text-lg font-normal text-black group-hover:text-offwhite mb-5'>{item.paragraph}</h4>
+                            {/* <Link href="#" className='text-lg font-semibold group-hover:text-white text-blue hover-underline'>
+                                {item.link}
+                                <ChevronRightIcon width={20} height={20} />
+                            </Link> */}
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            {/* content */}
+
             <>
   <div className="container mx-auto sm:px-4  ptb-120">
     <div
       className="flex flex-wrap  justify-center ptb-120 bg-cover items-center"
-      style={{ background: "url(../assets/img/roi-banner.webp)" }}
+      style={{ background: "url(../../../public/images/aboutus/roi-banner.webp)" }}
     >
       <div className="lg:w-3/4 pr-4 pl-4 text-center ">
         <h1 className=" fs-48 txt-blk fw-600 py-5">
@@ -77,7 +98,8 @@ const Aboutus = () => {
     </div>
     <div className="lg:w-1/3 pr-4 pl-4">
       <img
-        src="../assets/img/aboutus-2.png"
+        
+        src="../images/aboutus/aboutus-2.png"
         className="max-w-full h-auto"
         style={{ width: "auto" }}
       />
@@ -88,7 +110,7 @@ const Aboutus = () => {
     style={{ overflow: "hidden" }}
   >
     <div className="lg:w-1/3 pr-4 pl-4 py-5">
-      <img src="../assets/img/aboutus-4.png" className="max-w-full h-auto" />
+      <img src="../public/images/aboutus-2.png" className="max-w-full h-auto" />
     </div>
     <div
       className="lg:w-2/5 pr-4 pl-4 py-5 flex flex-col justify-center"
@@ -287,7 +309,7 @@ const Aboutus = () => {
       <div
         className="lg:w-1/2 pr-4 pl-4 hidden lg:block"
         style={{
-          background: "url(../assets/img/still-not-sure.webp)",
+          background: "url(../images/aboutus/aboutus-2.png)",
           borderRadius: 10,
           backgroundSize: "cover"
         }}
@@ -296,6 +318,7 @@ const Aboutus = () => {
   </div>
 </>
 
+            
         </div>
 
     )
