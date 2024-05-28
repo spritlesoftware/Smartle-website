1a9347b3c1711cb81984daf53bbba2920b1babe0
navanee1609 <navaneethanvs18@gmail.com>
2024-05-28 11:56:09 +0530

    Added Calendly inline widget and button click handlers to ROI page

--- app/roi/page.tsx
+++ app/roi/page.tsx
@@ -1,21 +1,32 @@
 "use client";
 
-import React, { useState } from "react";
-
+import React, { useState } from 'react';
+import { InlineWidget } from "react-calendly";
 const Roi = () => {
-  const [visitorsPM, setVisitorsPM] = useState("");
-  const [ordersPM, setOrdersPM] = useState("");
-  const [aov, setAov] = useState("");
-  const [results, setResults] = useState({
-    conversationsPerMonth: 0,
-    potentialAdditionalSales: 0,
-    potentialAOV: 0,
-    addUnlockSales: 0,
-    monthlyROI: 0,
-  });
+    const [showCalendly, setShowCalendly] = useState(false);
+    const [showsCalendly, setShowsCalendly] = useState(false);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+ 
+    const [visitorsPM, setVisitorsPM] = useState('');
+    const [ordersPM, setOrdersPM] = useState('');
+    const [aov, setAov] = useState('');
+    const [results, setResults] = useState({
+        conversationsPerMonth: 0,
+        potentialAdditionalSales: 0,
+        potentialAOV: 0,
+        addUnlockSales: 0,
+        monthlyROI: 0,
+    });
+
+    const handleButtonClick = () => {
+        setShowCalendly(true);
+      };
+    const handleButtonClicks = () => {
+        setShowCalendly(true);
+      };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
     if (!visitorsPM || !ordersPM || !aov) {
       console.log("error");
@@ -122,154 +133,115 @@ const Roi = () => {
                                         id="myButton"
                                     >
                                         Calculate
-                                    </button> */}
-                  <button
-                    className="text-sm md:text-xl font-semibold hover:shadow-xl text-white  px-6 md:py-4 md:px-14 rounded-full hover:bg-hoblue"
-                    style={{ backgroundColor: "#00356b", width: "100%" }}
-                    id="myButton"
-                  >
-                    Calculate
-                  </button>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                        <div className="lg:w-1/2 pr-4 pl-4">
+                            <div
+                                className="ms-3 my-2 px-2 pb-5 pt-3 bg-blue h-390"
+                                style={{ boxShadow: "0px 16px 48px 0px #D9D9D9", borderRadius: 10 }}
+                            >
+                                <h5 className="my-3 ps-3 fs-18 fw-600">ROI Calculator Result</h5>
+                                <ul className="flex flex-col pl-0 mb-0 border rounded border-gray-300 bg-blue">
+                                    <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
+                                        Conversations per month:
+                                        <span className="value float-end fs-20 fw-600" id="con-pm">
+                                            {results.conversationsPerMonth}
+                                        </span>
+                                    </li>
+                                    <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
+                                        Potential additional sales:
+                                        <span className="value float-end fs-20 fw-600" id="addSales">
+                                            {results.potentialAdditionalSales}
+                                        </span>
+                                    </li>
+                                    <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
+                                        Potential AOV from SmartleAI sales:
+                                        <span className="value float-end fs-20 fw-600" id="potAV">
+                                            $ {results.potentialAOV}
+                                        </span>
+                                    </li>
+                                    <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
+                                        Additional unlock sales from SmartleAI:
+                                        <span className="value float-end fs-20 fw-600" id="addSalesFromSpritle">
+                                            $ {results.addUnlockSales}
+                                        </span>
+                                    </li>
+                                    <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
+                                        Subscription Cost:
+                                        <span className="value float-end fs-20 fw-600" id="subsCost">
+                                            $ 15
+                                        </span>
+                                    </li>
+                                </ul>
+                                <div className="flex items-center justify-center">
+                                    <div
+                                        className="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded py-2 flex items-center justify-content-evenly bg-white w-90 text-left my-3 fs-20 hidden"
+                                        id="ROI"
+                                    >
+                                        Monthly ROI :{" "}
+                                        <span
+                                            id="mon-ROI"
+                                            className="float-end txt-green fw-600 fs-30"
+                                        >
+                                            {results.monthlyROI} %
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </form>
-            </div>
-            <div className="lg:w-1/2 pr-4 pl-4">
-              <div
-                className="ms-3 my-2 px-2 pb-5 pt-3 bg-blue h-390"
-                style={{
-                  boxShadow: "0px 16px 48px 0px #D9D9D9",
-                  borderRadius: 10,
-                }}
-              >
-                <h5 className="my-3 ps-3 fs-18 fw-600">
-                  ROI Calculator Result
-                </h5>
-                <ul className="flex flex-col pl-0 mb-0 rounded  bg-blue">
-                  <li className="relative block py-3 px-6 -mb-px bg-blue lh-2 flex items-center justify-between">
-                    Conversations per month:
-                    <span className="value float-end fs-20 fw-600" id="con-pm">
-                      {results.conversationsPerMonth}
-                    </span>
-                  </li>
-                  <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
-                    Potential additional sales:
-                    <span
-                      className="value float-end fs-20 fw-600"
-                      id="addSales"
-                    >
-                      {results.potentialAdditionalSales}
-                    </span>
-                  </li>
-                  <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
-                    Potential AOV from SmartleAI sales:
-                    <span className="value float-end fs-20 fw-600" id="potAV">
-                      $ {results.potentialAOV}
-                    </span>
-                  </li>
-                  <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 no-underline bg-blue lh-2 flex items-center justify-between">
-                    Additional unlock sales from SmartleAI:
-                    <span
-                      className="value float-end fs-20 fw-600 "
-                      id="addSalesFromSpritle"
-                    >
-                      $ {results.addUnlockSales}
-                    </span>
-                  </li>
-                  <li className="relative block py-3 px-6 -mb-px border border-r-0 border-l-0 border-gray-300 bg-blue lh-2 flex items-center justify-between" style={{borderBottom:".2rem"}}>
-                    Subscription Cost:
-                    <span
-                      className="value float-end fs-20 fw-600"
-                      id="subsCost"
-                    >
-                      $ 15
-                    </span>
-                  </li>
-                </ul>
-                <div className="flex items-center justify-center">
-                  <div
-                    className="inline-block p-1 text-center font-semibold text-sm align-baseline leading-none rounded py-2 flex items-center justify-content-evenly bg-white w-90 text-left my-3 fs-20 hidden"
-                    id="ROI"
-                  >
-                    Monthly ROI :{" "}
-                    <span
-                      id="mon-ROI"
-                      className="float-end txt-green fw-600 fs-30"
-                    >
-                      {results.monthlyROI} %
-                    </span>
-                  </div>
+                <div className="flex flex-wrap justify-center items-center mt-5">
+                    <div className="lg:w-1/3 pr-4 pl-4 text-center">
+                    <button
+        className="relative flex-grow max-w-full flex-1 px-4 inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded mb-5"
+        style={{ width: "90%" }}
+        onClick={handleButtonClick}
+      >
+        Unlock Additional Sales Now
+      </button>
+      {showCalendly && <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />}
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-        <div className="flex flex-wrap justify-center items-center mt-5 py-3">
-          <div className="lg:w-1/3 pr-4 pl-4 text-center">
-            {/* <button
-              className="relative flex-grow max-w-full flex-1 px-4 inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded mb-5"
-              style={{ width: "90%" }}
-              onClick={() =>
-                Calendly.initPopupWidget({
-                  url: "https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03",
-                })
-              }
-            >
-              Unlock Additional Sales Now
-            </button> */}
-            <button
-              className="text-sm md:text-xl font-semibold hover:shadow-xl text-white  px-6 md:py-4 md:px-14 rounded-full hover:bg-hoblue"
-              style={{ backgroundColor: "#00356b", width: "100%" }}
-            >
-              Unlock Additional Sales Now
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="container mx-auto sm:px-4 ptb-60">
-        <div
-          className="flex flex-wrap justify-center"
-          style={{ boxShadow: "0px 16px 48px 0px #D9D9D9", borderRadius: 10 }}
-        >
-          <div className="lg:w-1/2 pr-4 pl-4 p-12">
-            <h3 className="mb-3 txt-blk fs-40 fw-600 text-center">
-              Still Not Sure?
-            </h3>
-            <p className="txt-blk fs-18 text-center">
-              Let us show you a quick DEMO tailored to your store. You’ll be
-              impressed.
-            </p>
-            <div className="flex justify-center py-3">
-              {/* <button
-                className="w-70 inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded mt-4"
-                onClick={() =>
-                  Calendly.initPopupWidget({
-                    url: "https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03",
-                  })
-                }
-              >
-                Schedule Demo
-              </button> */}
-              <button
-                className="text-sm md:text-xl font-semibold hover:shadow-xl text-white  px-6 md:py-4 md:px-14 rounded-full hover:bg-hoblue"
-                style={{ backgroundColor: "#00356b" }}
-              >
-                Schedule Demo
-              </button>
+            <div className="container mx-auto sm:px-4 ptb-60">
+                <div
+                    className="flex flex-wrap justify-center"
+                    style={{ boxShadow: "0px 16px 48px 0px #D9D9D9", borderRadius: 10 }}
+                >
+                    <div className="lg:w-1/2 pr-4 pl-4 p-12">
+                        <h3 className="mb-3 txt-blk fs-40 fw-600 text-center">
+                            Still Not Sure?
+                        </h3>
+                        <p className="txt-blk fs-18 text-center">
+                            Let us show you a quick DEMO tailored to your store. You’ll be
+                            impressed.
+                        </p>
+                        <div className="flex justify-center">
+                        <button
+        className="w-70 inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded mt-4"
+        onClick={handleButtonClicks}
+      >
+        Schedule Demo
+      </button>
+      {showsCalendly && <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />}
+                        </div>
+                    </div>
+                    <div
+                        className="lg:w-1/2 pr-4 pl-4 hidden lg:block"
+                        style={{
+                            background:
+                                "url(https://smartle-ai.s3.amazonaws.com/assets/img/still-not-sure.webp)",
+                            borderRadius: 10,
+                            backgroundSize: "cover"
+                        }}
+                    />
+                </div>
             </div>
-          </div>
-          <div
-            className="lg:w-1/2 pr-4 pl-4 hidden lg:block"
-            style={{
-              background:
-                "url(https://smartle-ai.s3.amazonaws.com/assets/img/still-not-sure.webp)",
-              borderRadius: 10,
-              backgroundSize: "cover",
-            }}
-          />
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Roi;
