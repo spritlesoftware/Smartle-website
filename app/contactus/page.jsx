35fe00ec94c947eba2e556c79058f6e06491878a
navanee1609 <navaneethanvs18@gmail.com>
2024-05-30 04:07:16 -0700

    Added Supabase integration to contact us page, updated package-lock.json and package.json

--- app/contactus/page.jsx
+++ app/contactus/page.jsx
@@ -1,11 +1,21 @@
 "use client";
 import { useState } from "react";
 import { InlineWidget } from "react-calendly";
+import { createClient } from "@supabase/supabase-js";
 
+const supabaseUrl = "https://rjgkhjvxkvkcrqamlrns.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqZ2toanZ4a3ZrY3JxYW1scm5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE5NzI4MDQsImV4cCI6MjAyNzU0ODgwNH0.6gjvj2IYDuvvaCV0UDwZkVg51LJ7rvLx-CKT-ROpNqk"
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const Contactus = () => {
   const [showsCalendly, setShowsCalendly] = useState(false);
   const [overlayVisible, setOverlayVisible] = useState(false);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [textArea, setTextArea] = useState('');
+  const [errors, setErrors] = useState({});
 
   const handleButtonClicks = () => {
     setShowsCalendly(true);
@@ -16,141 +26,196 @@ const Contactus = () => {
     setShowsCalendly(false);
     setOverlayVisible(false);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let formErrors = {};
+
+    if (!firstName) formErrors.firstName = 'This is a required field';
+    if (!email) formErrors.email = 'This is a required field';
+    if (!textArea) formErrors.textArea = 'This is a required field';
+
+    setErrors(formErrors);
+
+    if (Object.keys(formErrors).length === 0) {
+      const { data, error } = await supabase.from('contactus').insert([
+        {
+          'First Name': firstName,
+          Email: email,
+          'About Site': textArea,
+          'Last Name': lastName,
+          'Phone Number': phoneNumber,
+        },
+      ]);
+
+      if (error) {
+        console.error('Error inserting data:', error.message);
+      } else {
+        // Clear the form
+        setFirstName('');
+        setLastName('');
+        setPhoneNumber('');
+        setEmail('');
+        setTextArea('');
+        setErrors({});
+
+        // Optional: Send email or other actions
+        fetch('https://rjgkhjvxkvkcrqamlrns.supabase.co/functions/v1/resend', {
+          mode: 'no-cors',
+        });
+      }
+    }
+  };
+
   return (
     <>
-      <div className="container mx-auto sm:px-4  ptb-120">
+      <div className="container mx-auto sm:px-4 ptb-120">
         <div
-          className="flex flex-wrap  justify-center ptb-120 bg-cover items-center"
+          className="flex flex-wrap justify-center ptb-120 bg-cover items-center"
           style={{ background: "url(images/roi-banner.webp)" }}
         >
-          <div className="lg:w-3/4 pr-4 pl-4 text-center ">
-            <h1 className=" fs-48 txt-blk fw-600" style={{ fontSize: 80 }}>
+          <div className="lg:w-3/4 pr-4 pl-4 text-center">
+            <h1 className="fs-48 txt-blk fw-600" style={{ fontSize: 80 }}>
               Get in touch!
             </h1>
             <p className="text-center my-2">
-              {" "}
               Drop us a message and sit back for the magic to happen
             </p>
             <div className="flex items-center justify-center">
-      <button
-        className="inline-block text-center select-none border font-normal whitespace-no-wrap rounded py-3 px-3 leading-normal no-underline btn-dark-blue btn-rounded"
-        onClick={handleButtonClicks}
-      >
-        Try Smartle AI
-      </button>
-      {showsCalendly && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
-          <div className="relative rounded-lg p-8 w-11/12 max-w-4xl my-8 bg-transparent">
-            <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl text-white"
-              onClick={handleCalendlyClose}
-            >
-              &#10005; {/* X button */}
-            </button>
-            {/* Include your InlineWidget component here */}
-            <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />
-          </div>
-        </div>
-      )}
-      {overlayVisible && (
-        <div
-          className="fixed inset-0 z-40"
-          style={{
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            // backdropFilter: 'blur(5px)',
-          }}
-          onClick={handleCalendlyClose}
-        />
-      )}
-    </div>
+              <button
+                className="inline-block text-center select-none border font-normal whitespace-no-wrap rounded py-3 px-3 leading-normal no-underline btn-dark-blue btn-rounded"
+                onClick={handleButtonClicks}
+              >
+                Try Smartle AI
+              </button>
+              {showsCalendly && (
+                <div className="fixed inset-0 flex items-center justify-center z-50">
+                  <div className="relative rounded-lg p-8 w-11/12 max-w-4xl my-8 bg-transparent">
+                    <button
+                      className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl text-white"
+                      onClick={handleCalendlyClose}
+                    >
+                      &#10005; {/* X button */}
+                    </button>
+                    <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />
+                  </div>
+                </div>
+              )}
+              {overlayVisible && (
+                <div
+                  className="fixed inset-0 z-40"
+                  style={{
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                  }}
+                  onClick={handleCalendlyClose}
+                />
+              )}
+            </div>
           </div>
         </div>
       </div>
+
       <div className="container mx-auto px-4 lg:flex lg:space-x-4">
-  <div className="lg:w-3/5 mb-4 lg:mb-0">
-    <div
-      className="py-5 px-5 rounded-lg shadow-lg h-full"
-      style={{ boxShadow: "0px 16px 48px 0px #D9D9D9" }}
-    >
-      <h1 className="text-center font-semibold text-3xl py-3">Contact us</h1>
+        <div className="lg:w-3/5 mb-4 lg:mb-0">
+          <div
+            className="py-5 px-5 rounded-lg shadow-lg h-full"
+            style={{ boxShadow: "0px 16px 48px 0px #D9D9D9" }}
+          >
+            <h1 className="text-center font-semibold text-3xl py-3">Contact us</h1>
+            <form onSubmit={handleSubmit}>
+              <div className="flex flex-wrap justify-between mb-3 space-y-4 lg:space-y-0">
+                <div className="w-full lg:w-2/5">
+                  <label htmlFor="FirstName" className="mb-2 block">
+                    First Name *
+                  </label>
+                  <input
+                    className="rounded-lg border w-full p-2"
+                    style={{ borderColor: "lightgrey" }}
+                    type="text"
+                    id="FirstName"
+                    name="name"
+                    placeholder="First name"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                  <p className="text-red-500 text-xs mt-1" id="FirstNameErr">{errors.firstName}</p>
+                </div>
+                <div className="w-full lg:w-2/5">
+                  <label htmlFor="LastName" className="mb-2 block">
+                    Last Name
+                  </label>
+                  <input
+                    className="rounded-lg border w-full p-2"
+                    style={{ borderColor: "lightgrey" }}
+                    type="text"
+                    id="LastName"
+                    name="name"
+                    placeholder="Last name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                  />
+                </div>
+              </div>
 
-      <div className="flex flex-wrap justify-between mb-3 space-y-4 lg:space-y-0">
-        <div className="w-full lg:w-2/5">
-          <label htmlFor="FirstName" className="mb-2 block">
-            First Name *
-          </label>
-          <input
-            className="rounded-lg border  w-full p-2" style={{borderColor:"lightgrey"}}
-            type="text"
-            id="FirstName"
-            name="name"
-            placeholder="First name"
-          />
-          <p className="text-red-500 text-xs mt-1" id="FirstNameErr"></p>
-        </div>
-        <div className="w-full lg:w-2/5">
-          <label htmlFor="LastName" className="mb-2 block">
-            Last Name
-          </label>
-          <input
-            className="rounded-lg border w-full p-2" style={{borderColor:"lightgrey"}}
-            type="text"
-            id="LastName"
-            name="name"
-            placeholder="Last name"
-          />
-        </div>
-      </div>
+              <div className="flex flex-wrap justify-between mb-4 space-y-4 lg:space-y-0">
+                <div className="w-full lg:w-2/5">
+                  <label htmlFor="Email" className="mb-2 block">
+                    Email*
+                  </label>
+                  <input
+                    className="rounded-lg border w-full p-2 block"
+                    style={{ borderColor: "lightgrey" }}
+                    type="email"
+                    id="Email"
+                    name="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <p className="text-red-500 text-xs mt-1" id="EmailErr">{errors.email}</p>
+                </div>
+                <div className="w-full lg:w-2/5">
+                  <label htmlFor="PhoneNumber" className="mb-2 block">
+                    Phone Number
+                  </label>
+                  <input
+                    className="rounded-lg border w-full p-2"
+                    style={{ borderColor: "lightgrey" }}
+                    type="tel"
+                    id="PhoneNumber"
+                    name="phone"
+                    placeholder="Phone number"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                  />
+                </div>
+              </div>
 
-      <div className="flex flex-wrap justify-between mb-4 space-y-4 lg:space-y-0">
-        <div className="w-full lg:w-2/5">
-          <label htmlFor="Email" className="mb-2 block">
-            Email*
-          </label>
-          <input
-            className="rounded-lg border  w-full p-2 block" style={{borderColor:"lightgrey"}}
-            type="email"
-            id="Email"
-            name="email"
-            placeholder="Email"
-          />
-          <p className="text-red-500 text-xs mt-1" id="EmailErr"></p>
-        </div>
-        <div className="w-full lg:w-2/5">
-          <label htmlFor="PhoneNumber" className="mb-2 block">
-            Phone Number
-          </label>
-          <input
-            className="rounded-lg border  w-full p-2" style={{borderColor:"lightgrey"}}
-            type="tel"
-            id="PhoneNumber"
-            name="phone"
-            placeholder="Phone number"
-          />
-        </div>
-      </div>
+              <div className="mb-4">
+                <label htmlFor="text-area" className="mb-2 block">
+                  Tell us a little about your site*
+                </label>
+                <textarea
+                  className="rounded-lg border border-gray-300 w-full p-2 block resize-y"
+                  id="text-area"
+                  placeholder="Write your message"
+                  style={{ height: "170px", borderColor: 'lightgrey' }}
+                  value={textArea}
+                  onChange={(e) => setTextArea(e.target.value)}
+                ></textarea>
+                <p className="text-red-500 text-xs mt-1" id="TextAreaErr">{errors.textArea}</p>
+              </div>
 
-      <div className="mb-4">
-        <label htmlFor="text-area" className="mb-2 block">
-          Tell us a little about your site*
-        </label>
-        <textarea
-          className="rounded-lg border border-gray-300 w-full p-2 block resize-y" // Added 'resize-y' class to enable vertical resizing
-          id="text-area"
-          placeholder="Write your message"
-          style={{ height: "170px", borderColor:'lightgrey'}} // Adjust the height as needed
-        ></textarea>
-        <p className="text-red-500 text-xs mt-1" id="TextAreaErr"></p>
-      </div>
-
-      <button
-        className="text-white font-bold py-3 px-4 rounded-3xl w-full"
-        id="submit" style={{background:' #00356b'}}
-      >
-        Submit
-      </button>
-    </div>
-  </div>
+              <button
+                className="text-white font-bold py-3 px-4 rounded-3xl w-full"
+                id="submit"
+                style={{ background: '#00356b' }}
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
 
   <div className="lg:w-2/5 lg:pl-4 py-2 lg:py-0 flex flex-col justify-center">
     <div>
