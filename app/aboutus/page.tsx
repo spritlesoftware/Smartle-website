4df005eb875deb7222131ef61accacd733325fe5
navanee1609 <navaneethanvs18@gmail.com>
2024-08-02 10:32:48 +0530

    Replaced carousel and then svg with images

--- app/aboutus/page.tsx
+++ app/aboutus/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import Image from "next/image";
-import Link from "next/link";
-import { ChevronRightIcon } from "@heroicons/react/20/solid";
 
 import React, { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import { Navigation, Pagination } from 'swiper/modules';
+import { Pagination } from 'swiper/modules';
 import { InlineWidget } from 'react-calendly';
+import { FaSmile, FaHandSparkles, FaChartLine, FaClock, FaDatabase } from 'react-icons/fa';
 
 interface datatype {
   heading: string;
@@ -46,37 +44,37 @@ const Aboutus = () => {
   const [overlayVisible, setOverlayVisible] = useState(false);
 
 
-   const handleButtonClicks = () => {
+  const handleButtonClicks = () => {
     setShowsCalendly(true);
     setOverlayVisible(true);
   };
 
   const handleCalendlyClose = () => {
     setShowsCalendly(false);
-        setOverlayVisible(false);
+    setOverlayVisible(false);
   };
-  
+
 
   const handleButtonClick = () => {
     setShowCalendly(true);
     setOverlayVisible(true);
-};
+  };
 
-const handleCloseCalendly = () => {
+  const handleCloseCalendly = () => {
     setShowCalendly(false);
     setOverlayVisible(false);
-};
+  };
 
-  
-  
-  
+
+
+
   return (
     <div>
-     
+
       <>
-      
+
         <div className="container mx-auto sm:px-4  ptb-120">
-        {overlayVisible && <div className="overlay"></div>}
+          {overlayVisible && <div className="overlay"></div>}
           <div
             className="flex flex-wrap  justify-center ptb-120 bg-cover items-center"
             style={{ background: "url(https://smartle-ai.s3.amazonaws.com/assets/img/roi-banner.webp)" }}
@@ -95,44 +93,44 @@ const handleCloseCalendly = () => {
               >
                 Try Smartle AI For FREE
               </button> */}
-               <div className="flex items-center justify-center">
-            <button
-                className="inline-block text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded mt-3"
-                onClick={handleButtonClick}
-            >
-                Try Smartle AI For FREE
-            </button>
-            {showCalendly && (
-                <div className="fixed inset-0 flex items-center justify-center z-50">
+              <div className="flex items-center justify-center">
+                <button
+                  className="inline-block text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded mt-3"
+                  onClick={handleButtonClick}
+                >
+                  Try Smartle AI For FREE
+                </button>
+                {showCalendly && (
+                  <div className="fixed inset-0 flex items-center justify-center z-50">
                     <div className="relative rounded-lg p-8 w-11/12 max-w-4xl my-8 bg-transparent">
-                        <button
-                            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl text-white"
-                            onClick={handleCloseCalendly}
-                        >
-                            &#10005; {/* X button */}
-                        </button>
-                        <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />
+                      <button
+                        className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl text-white"
+                        onClick={handleCloseCalendly}
+                      >
+                        &#10005; {/* X button */}
+                      </button>
+                      <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />
                     </div>
-                </div>
-            )}
-            {overlayVisible && (
-                <div
+                  </div>
+                )}
+                {overlayVisible && (
+                  <div
                     className="fixed inset-0 bg-gray-800 bg-opacity-50 z-40"
                     onClick={handleCloseCalendly}
-                />
-            )}
-        </div>
+                  />
+                )}
+              </div>
 
-             
+
             </div>
           </div>
         </div>
         <div className="container mx-auto sm:px-4 flex lg:flex-row flex-wrap justify-between w-full">
           <div className="lg:w-3/5 pr-4 pl-4">
-            <h2 style={{ textAlign: "left" , fontWeight: 500}} className="fw-bold text-5xl fs-36">
+            <h2 style={{ textAlign: "left", fontWeight: 500 }} className="fw-bold text-5xl fs-36">
               Your AI partner
             </h2>
-            <p style={{ textAlign: "left" , fontSize:"1.15rem"}} className="py-3">
+            <p style={{ textAlign: "left", fontSize: "1.15rem" }} className="py-3">
               Imagine a world where conversation isn't confined to screens and
               buttons. Where you can chat, ask questions, and get things done
               with an AI as natural and engaging as a real person. That's the
@@ -166,133 +164,122 @@ const handleCloseCalendly = () => {
             className="lg:w-2/5 pr-4 pl-4 py-5 flex flex-col justify-center"
             style={{ marginLeft: "2%" }}
           >
-            <h2 className="container fw-bold text-5xl fs-36 mb-2" style={{ fontWeight: 500}}>Smartle AI</h2>
-            <h6 style={{ fontWeight: 400 , fontSize:"1rem" }}>
+            <h2 className="container fw-bold text-5xl fs-36 mb-2" style={{ fontWeight: 500 }}>Smartle AI</h2>
+            <h6 style={{ fontWeight: 400, fontSize: "1.15rem" }} className="py-3" >
               Continuously learning, it adopts new strategies, while
               <br />
               personalizing its interactions to cater to your unique style and
-              <br />
               needs, ensuring a seamless and tailored experience.
             </h6>
           </div>
         </div>
 
-      
-
-
-<div className="container mx-auto bg-lightgrey sm:px-4 pt-12 pb-12">
-      <div className="flex flex-wrap items-center justify-center">
-        <h3 className="text-center text-4xl font-semibold mb-8 w-full">
-          Why customers win with Smartle?
-        </h3>
-        <div className="w-full lg:w-5/6 pr-4 pl-4">
-          <Swiper
-            spaceBetween={2}
-            slidesPerView={1}
-            breakpoints={{
-              640: {
-                slidesPerView: 1,
-                spaceBetween: 5,
-              },
-              768: {
-                slidesPerView: 2,
-                spaceBetween: 10,
-              },
-              1024: {
-                slidesPerView: 3,
-                spaceBetween: 15,
-              },
-            }}
-            className="swiper"
-            id="owl-carousell"
-          >
-           
-           
-            <SwiperSlide>
-              <div className="bg-white p-6 rounded-md" style={{ maxWidth: 320, height:320 }}>
-                <div className="text-center w-full font-semibold text-gray-900 mt-4">
-                  <h3 className="text-2xl mb-3 text-black">Happier  <br /> Shoppers</h3>
-                
-            
-                  <hr className="mx-auto" style={{ width: '80%', color:"lightgrey" , }} />
+        <div className="container mx-auto sm:px-4 pt-12 pb-12">
+          <div className="flex flex-wrap items-center justify-center bg-gradient-to-r from-[#E3F2FD] to-[#FCE4EC] p-6 rounded-md">
+            <h3 className="text-center text-4xl font-semibold mb-8 w-full">
+              Why customers win with Smartle?
+            </h3>
+            <Swiper
+              spaceBetween={20}
+              slidesPerView={1}
+              navigation={false}
+              pagination={{ clickable: true }}
+              breakpoints={{
+                640: {
+                  slidesPerView: 1,
+                  spaceBetween: 10,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 20,
+                },
+                1024: {
+                  slidesPerView: 3,
+                  spaceBetween: 30,
+                },
+              }}
+              modules={[Pagination]}
+              className="swiper"
+              effect="slide"
+            >
+              <SwiperSlide>
+                <div className="bg-[#AFD5F0] p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 duration-300 ease-in-out flex flex-col items-center">
+                  <div className="flex items-center mb-4">
+                    <FaSmile className="text-2xl text-black mr-4" />
+                    <div className="text-center">
+                      <h3 className="text-2xl mb-2">Happier <br /> Shoppers</h3>
+                      <hr className="mx-auto my-2 w-1/2 border-gray-300 border-opacity-30" />
+                    </div>
+                  </div>
+                  <p className="text-center py-2 text-black">
+                    Smartle AI provides delightful conversations, making shopping experiences more engaging and enjoyable.
+                  </p>
                 </div>
-                <p className="text-center py-3 text-black">
-                  Smartle AI provides delightful conversations, making shopping experiences more engaging and enjoyable.
-                </p>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="bg-light bg-white p-6 rounded-md" style={{ maxWidth: 320, height:320 }}>
-                <div className="text-center w-full font-semibold text-gray-900 mt-4">
-                  <h3 className="text-2xl mb-3 text-black">
-                  Effortless
-                <br />
-                Assistance
-
-                  </h3>
-                  <hr className="mx-auto" style={{ width: '80%', color:"lightgrey" , }} />
+              <SwiperSlide>
+                <div className="bg-[#F8C8DC] p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 duration-300 ease-in-out flex flex-col items-center">
+                  <div className="flex items-center mb-4">
+                    <FaHandSparkles className="text-2xl text-black mr-4" />
+                    <div className="text-center">
+                      <h3 className="text-2xl mb-2">Effortless <br /> Assistance</h3>
+                      <hr className="mx-auto my-2 w-1/2 border-gray-300 border-opacity-30" />
+                    </div>
+                  </div>
+                  <p className="text-center py-2 text-black">
+                    Get instant answers and product recommendations, leading to a smoother shopping experience.
+                  </p>
                 </div>
-                <p className="text-center text-black py-3">
-                Get instant answers and product recommendations, leading to faster purchases and fewer frustrated customers.
-                </p>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="bg-light bg-white p-6 rounded-md" style={{ maxWidth: 320, height:320 }}>
-                <div className="text-center w-full font-semibold text-gray-900 mt-4">
-                  <h3 className="text-2xl mb-3 text-black">
-                  Personalized
-                <br />
-                Touch
-
-                  </h3>
-                  <hr className="mx-auto" style={{ width: '80%', color:"lightgrey" , }} />
+              <SwiperSlide>
+                <div className="bg-[#FEDD9E] p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 duration-300 ease-in-out flex flex-col items-center">
+                  <div className="flex items-center mb-4">
+                    <FaChartLine className="text-2xl text-black mr-4" />
+                    <div className="text-center">
+                      <h3 className="text-2xl mb-2">Increased <br /> Efficiency</h3>
+                      <hr className="mx-auto my-2 w-1/2 border-gray-300 border-opacity-30" />
+                    </div>
+                  </div>
+                  <p className="text-center py-2 text-black">
+                    Smartle AI handles repetitive tasks, allowing your team to focus on more critical aspects of your business.
+                  </p>
                 </div>
-                <p className="text-center text-black py-3">
-                Smartle AI learns buyer preferences, offering relevant suggestions and boosting customer satisfaction.
-                </p>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="bg-light bg-white p-6 rounded-md" style={{ maxWidth: 320, height:320 }}>
-                <div className="text-center w-full font-semibold text-gray-900 mt-4">
-                  <h3 className="text-2xl mb-3 text-black">
-                  24/7 support
-                <br />
-                  </h3>
-                  <hr className="mx-auto" style={{ width: '80%', color:"lightgrey" , }} />
+              <SwiperSlide>
+                <div className="bg-[#AFD5F0] p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 duration-300 ease-in-out flex flex-col items-center">
+                  <div className="flex items-center mb-4">
+                    <FaClock className="text-2xl text-black mr-4" />
+                    <div className="text-center">
+                      <h3 className="text-2xl mb-2">24/7 <br /> Availability</h3>
+                      <hr className="mx-auto my-2 w-1/2 border-gray-300 border-opacity-30" />
+                    </div>
+                  </div>
+                  <p className="text-center py-2 text-black">
+                    With Smartle AI, you’re never alone. Your virtual assistant is available round-the-clock 24/7.
+                  </p>
                 </div>
-                <p className="text-center text-black py-3">
-                With round-the-clock AI assistance that continues conversations even after business hours, you'll never miss a sale.
-                </p>
-              </div>
-            </SwiperSlide>
+              </SwiperSlide>
 
-            <SwiperSlide>
-              <div className="bg-light bg-white p-6 rounded-md" style={{ maxWidth: 320, height:320 }}>
-                <div className="text-center w-full font-semibold text-gray-900 mt-4">
-                  <h3 className="text-2xl mb-3 text-black">
-                  Data-Driven 
-                <br />
-                Insights
-                  </h3>
-                  <hr className="mx-auto" style={{ width: '80%', color:"lightgrey" , }} />
+              <SwiperSlide>
+                <div className="bg-[#F8C8DC] p-6 rounded-lg shadow-lg transition-transform transform hover:scale-105 duration-300 ease-in-out flex flex-col items-center">
+                  <div className="flex items-center mb-4">
+                    <FaDatabase className="text-2xl text-black mr-4" />
+                    <div className="text-center">
+                      <h3 className="text-2xl mb-2">Data-Driven <br /> Insights</h3>
+                      <hr className="mx-auto my-2 w-1/2 border-gray-300 border-opacity-30" />
+                    </div>
+                  </div>
+                  <p className="text-center py-2 text-black">
+                    Through Smartle AI interactions, obtain useful consumer data that can be used to improve marketing strategies and product suggestions.
+                  </p>
                 </div>
-                <p className="text-center text-black py-3">
-                Through Smartle AI interactions, obtain useful consumer data that can be used to improve marketing strategies and product suggestions.
-                </p>
-              </div>
-            </SwiperSlide>
-
-            
-           
-          </Swiper>
+              </SwiperSlide>
+            </Swiper>
+          </div>
         </div>
-      </div>
-    </div>
+
 
 
 
@@ -311,32 +298,32 @@ const handleCloseCalendly = () => {
                 impressed.
               </p>
               <div className="flex items-center justify-center">
-            <button
-                className="my-3 inline-block text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded"
-                onClick={handleButtonClicks}
-            >
-                Schedule Demo
-            </button>
-            {showsCalendly && (
-                <div className="fixed inset-0 flex items-center justify-center z-50">
+                <button
+                  className="my-3 inline-block text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline btn-dark-blue btn-rounded"
+                  onClick={handleButtonClicks}
+                >
+                  Schedule Demo
+                </button>
+                {showsCalendly && (
+                  <div className="fixed inset-0 flex items-center justify-center z-50">
                     <div className="relative rounded-lg p-8 w-11/12 max-w-4xl my-8 bg-transparent">
-                        <button
-                            className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl text-white"
-                            onClick={handleCalendlyClose}
-                        >
-                            &#10005; {/* X button */}
-                        </button>
-                        <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />
+                      <button
+                        className="absolute top-2 right-2 text-gray-500 hover:text-gray-700 text-2xl text-white"
+                        onClick={handleCalendlyClose}
+                      >
+                        &#10005; {/* X button */}
+                      </button>
+                      <InlineWidget url="https://calendly.com/prabhu-m-spritle/smartle-ai-demo?month=2024-03" />
                     </div>
-                </div>
-            )}
-            {overlayVisible && (
-                <div
+                  </div>
+                )}
+                {overlayVisible && (
+                  <div
                     className="fixed inset-0 bg-gray-800 bg-opacity-50 z-40"
                     onClick={handleCalendlyClose}
-                />
-            )}
-        </div>
+                  />
+                )}
+              </div>
             </div>
             <div
               className="lg:w-1/2 pr-4 pl-4 hidden lg:block"
